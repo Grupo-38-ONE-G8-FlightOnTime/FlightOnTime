c7b58339bda0bc8e66175ce20b93fb1daf74ab8d
Ricardo Hamada <vitor_hamada@yahoo.com.br>
2026-01-15 10:50:12 -0300

    Add backend source files

---
+++ backend/src/main/java/br/com/grupo_38/flight_on_time/dto/PredictRequest.java
@@ -0,0 +1,60 @@
+package br.com.grupo_38.flight_on_time.dto;
+
+public class PredictRequest {
+    private String flightNumber;
+    private String departureAirport;
+    private String arrivalAirport;
+    private String date;
+    private int distance;
+
+    public PredictRequest() {
+    }
+
+    public PredictRequest(String flightNumber, String departureAirport, String arrivalAirport, String date, int distance) {
+        this.flightNumber = flightNumber;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.date = date;
+        this.distance = distance;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+}
